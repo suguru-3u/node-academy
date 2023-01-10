c7b75631e5c384e9828476e89077bccc83ebe6fb
suguru-3u <suguru.13579515@gmail.com>
2023-01-11 07:12:53 +0900

    study node eventEmitter

--- EventEmitter/index.js
+++ EventEmitter/index.js
@@ -1,12 +1,40 @@
 const EventEmitter = require("events");
-const myEventter = new EventEmitter();
+const eventEmitter = new EventEmitter();
 
 // イベントの登録＆処理
-myEventter.on("event", () => {
+eventEmitter.on("event", () => {
   console.log("B");
 });
 
 // イベントの発火
 console.log("A");
-myEventter.emit("event");
+eventEmitter.emit("event");
 console.log("C");
+
+eventEmitter.on("box", () => {
+  console.log("listener1");
+  console.log("Sum of this listener is " + eventEmitter.listenerCount("box"));
+});
+
+const listenerMessage1 = () => console.log("This is listener1");
+const listenerMessage2 = () => console.log("This is listener2");
+
+eventEmitter.on("addItem1", () => {
+  console.log("add listener");
+  eventEmitter.addListener("box", listenerMessage1);
+});
+
+eventEmitter.on("addItem2", () => {
+  console.log("add listener");
+  eventEmitter.addListener("box", listenerMessage2);
+});
+
+eventEmitter.on("removeItem", () => {
+  console.log("remove listener");
+  eventEmitter.removeListener("box", listenerMessage1);
+});
+
+eventEmitter.emit("addItem1");
+eventEmitter.emit("addItem2");
+eventEmitter.emit("removeItem");
+eventEmitter.emit("box");
